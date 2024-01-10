5e0cc6b4b9424e6c94eebbae6de6ae7ecd34105f
22email <22imgs21212222@gmail.com>
2024-01-09 23:56:14 -0500

    Scanner Finished

--- src/main/java/com/keviny/egg/EggScanner.java
+++ src/main/java/com/keviny/egg/EggScanner.java
@@ -1,7 +1,9 @@
 package com.keviny.egg;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class EggScanner {
   private final String source;
@@ -10,6 +12,35 @@ public class EggScanner {
   private int start = 0; // First character of lexeme
   private int current = 0; // Current character of lexeme
   private int line = 1;
+
+  private static final Map<String, TokenType> keywords;
+
+  static {
+    keywords = new HashMap<>();
+    keywords.put("and", TokenType.AND);
+    keywords.put("class", TokenType.CLASS);
+    keywords.put("else", TokenType.ELSE);
+    keywords.put("false", TokenType.FALSE);
+    keywords.put("for", TokenType.FOR);
+    keywords.put("fun", TokenType.FUN);
+    keywords.put("if", TokenType.IF);
+    keywords.put("NOTHING", TokenType.NOTHING);
+    keywords.put("or", TokenType.OR);
+    keywords.put("print", TokenType.PRINT);
+    keywords.put("return", TokenType.RETURN);
+    keywords.put("super", TokenType.SUPER);
+    keywords.put("this", TokenType.THIS);
+    keywords.put("true", TokenType.TRUE);
+    keywords.put("var", TokenType.VAR);
+    keywords.put("while", TokenType.WHILE);
+
+    // Graphics:
+    keywords.put("pen_down", TokenType.PEN_DOWN);
+    keywords.put("pen_up", TokenType.PEN_UP);
+    keywords.put("pen_color", TokenType.PEN_COLOR);
+    keywords.put("pen_go", TokenType.PEN_GO);
+    keywords.put("pen_rotate", TokenType.PEN_ROTATE);
+  }
 
   public EggScanner(String source) {
     this.source = source;
@@ -76,14 +107,41 @@ public class EggScanner {
       case '>':
         addToken(matchNext('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER);
         break;
-      default:
-        Egg.error(line, "Unexpected Character");
+      case '/':
+        if (matchNext('/')) {
+          // Continue reading until the end of line or file when two slashes (comment) are
+          // read & do nothing
+          while (peek() != '\n' && !isAtEnd()) advance();
+        } else {
+          addToken(TokenType.SLASH);
+        }
         break;
+      case ' ': // Useless characters since anguage won't be whitespace dependent
+      case '\r':
+      case '\t':
+        break;
+      case '\n':
+        line++;
+        break;
+      case '"':
+        addString();
+        break;
+
+      default:
+        if (isDigit(c)) {
+          addNumber();
+
+        } else if (isAlpha(c)) {
+          addIdentifierOrKeyword();
+        } else {
+          Egg.error(line, "Unexpected Character");
+          break;
+        }
     }
   }
 
   private char advance() {
-    // Return current, then increment it 
+    // Return current, then increment it
     return source.charAt(current++);
   }
 
@@ -102,5 +160,68 @@ public class EggScanner {
 
     current++;
     return true;
+  }
+
+  private char peek() {
+    // Checks the next character
+    // Called "lookahead"
+    if (isAtEnd()) return '\0';
+    return source.charAt(current);
+  }
+
+  private char peekNext() {
+    if (current + 1 >= source.length()) return '\0';
+    return source.charAt(current + 1);
+  }
+
+  private boolean isDigit(char c) {
+    return c >= '0' && c <= '9';
+  }
+
+  private boolean isAlpha(char c) {
+    return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c == '_'));
+  }
+
+  private boolean isAlphaNumeric(char c) {
+    return isDigit(c) || isAlpha(c);
+  }
+
+  private void addString() {
+    while (peek() != '"' && !isAtEnd()) {
+      if (peek() == '\n') line++;
+      advance();
+    }
+
+    if (isAtEnd()) {
+      Egg.error(line, "Unterminated String");
+    }
+
+    // Still want to advance over the closing " even though it gets by the substring
+    // method call
+    // This is because it is necessary to consider the lexeme (which could be "foo")
+    advance();
+    String literal = source.substring(start + 1, current - 1);
+    addToken(TokenType.STRING, literal);
+  }
+
+  private void addNumber() {
+    while (isDigit(peek())) advance();
+
+    if (peek() == '.' && isDigit(peekNext())) {
+      // Include the decimal
+      advance();
+      while (isDigit(peek())) advance();
+    }
+
+    addToken(TokenType.NUMBER, Double.parseDouble(source.substring(start, current)));
+  }
+
+  private void addIdentifierOrKeyword() {
+    while (isAlphaNumeric(peek())) advance();
+
+    String text = source.substring(start, current);
+    TokenType type = keywords.get(text);
+    if (type == null) type = TokenType.IDENTIFIER;
+    addToken(type);
   }
 }
--- src/main/java/com/keviny/egg/TokenType.java
+++ src/main/java/com/keviny/egg/TokenType.java
@@ -2,14 +2,14 @@ package com.keviny.egg;
 
 public enum TokenType {
   // Single character
-  LEFT_PAREN, 
+  LEFT_PAREN,
   RIGHT_PAREN,
 
   LEFT_BRACE,
   RIGHT_BRACE,
 
   COMMA,
-  PERIOD, 
+  PERIOD,
   MINUS,
   PLUS,
   SEMICOLON,
@@ -17,17 +17,17 @@ public enum TokenType {
   STAR,
 
   // One or two character tokens
-  BANG, 
-  BANG_EQUAL, 
+  BANG,
+  BANG_EQUAL,
 
   EQUAL,
   EQUAL_EQUAL,
 
-  GREATER, 
-  GREATER_EQUAL, 
+  GREATER,
+  GREATER_EQUAL,
 
-  LESS, 
-  LESS_EQUAL, 
+  LESS,
+  LESS_EQUAL,
 
   // Literals
   IDENTIFIER,
@@ -37,7 +37,7 @@ public enum TokenType {
   NUMBER,
 
   // Keywords
-  // Consider removing class and other unecessary features? 
+  // Consider removing class and other unecessary features?
   AND,
   OR,
   CLASS,
@@ -60,15 +60,15 @@ public enum TokenType {
   SUPER,
   THIS,
 
-  // TODO: Graphics
-  // Intrinsic method to represent pen drawing
+  // Intrinsic methods to represent pen drawing
   // Only one pen can exist at a time
-  // PEN_DOWN
-  // PEN_UP
-  // PEN_COLOR
+  PEN_DOWN,
+  PEN_UP,
+  PEN_COLOR,
+  PEN_GO,
+  PEN_ROTATE,
+  // TODO: Hard stuff
   // PEN_MOVE -> linearly interpolated (might be hard)
-  // PEN_GO -> teleporting
-  // PEN_ROTATE
   // PEN_SPEED -> this might be hard
 
   EOF
